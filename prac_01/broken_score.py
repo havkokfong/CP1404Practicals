b22fd6b31c2bac620a0930ea8959a6d6ff720be3
Hav Kokfong <havkokfong@my.jcu.edu.au>
2018-11-20 15:29:43 +0800

    add function

--- prac_01/broken_score.py
+++ prac_01/broken_score.py
@@ -3,14 +3,21 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Pass")
-elif score <= 100:
-    print("Excellent")
-else:
-    print("Invalid score")
+def main():
+    score = float(input("Enter score: "))
+    cal_score(score)
+
+
+def cal_score(score):
+    if score < 0:
+        print("Invalid score")
+    elif score < 50:
+        print("Bad")
+    elif score < 90:
+        print("Pass")
+    elif score <= 100:
+        print("Excellent")
+    else:
+        print("Invalid score")
+
+main()
