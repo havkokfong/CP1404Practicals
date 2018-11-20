6acba5260d100374bbfb24bb990f45e19b87969f
Hav Kokfong <havkokfong@my.jcu.edu.au>
2018-11-20 15:54:58 +0800

    starter code

---
+++ prac_03/gps.py
@@ -0,0 +1,17 @@
+
+import random
+
+POPULATION = 1000
+
+
+def main():
+    print("Welcome to the Gopher Population Simulator")
+    print("Starting population: ", POPULATION)
+    while
+    year = 0
+    for i in range[1:11]:
+        year += 1
+
+    print(year)
+
+main()
