50b5841b9f109c0ad671714ade828ae67f7c2480
Hav Kokfong <havkokfong@my.jcu.edu.au>
2018-11-20 15:39:03 +0800

    Refactor

--- prac_02/ascii_table.py
+++ prac_02/ascii_table.py
@@ -1,8 +1,21 @@
-char = input("Enter a character: ")
-print("The ASCII code for {} is {}".format(char,ord(char)))
-num = int(input("Enter a number between 33 and 127: "))
-while num < 33 or num > 127:
-    print("Invalid number")
+
+def main():
+
+    char = input("Enter a character: ")
+    while char > 1:
+        print("Invalid letter")
+        char = input("Enter a character: ")
+    print("The ASCII code for {} is {}".format(char,ord(char)))
+    get_number()
+
+
+def get_number():
     num = int(input("Enter a number between 33 and 127: "))
-else:
-    print("The character for {} is {}".format(num, chr(num)))
+    while num < 33 or num > 127:
+        print("Invalid number")
+        num = int(input("Enter a number between 33 and 127: "))
+    else:
+        print("The character for {} is {}".format(num, chr(num)))
+
+
+main()
