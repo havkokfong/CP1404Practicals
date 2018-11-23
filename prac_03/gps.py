88310886245885146c8260e9561b9337fe8b2f43
Hav Kokfong <havkokfong@my.jcu.edu.au>
2018-11-23 17:27:26 +0800

    Finish

--- prac_03/gps.py
+++ prac_03/gps.py
@@ -7,11 +7,18 @@ POPULATION = 1000
 def main():
     print("Welcome to the Gopher Population Simulator")
     print("Starting population: ", POPULATION)
-    while
     year = 0
-    for i in range[1:11]:
+
+    while year <= 9:
+        born_rate = random.randrange(10, 20)
+        die_rate = random.randint(5, 25)
+        born = POPULATION * born_rate // 100
+        dead = POPULATION * die_rate // 100
+        new_population = POPULATION + born - dead
         year += 1
-
-    print(year)
-
+        print("Year: ", year)
+        print("********")
+        print(born, "gophers were born.", dead, "died.")
+        print("Population: ",  new_population)
+        print("\n")
 main()
