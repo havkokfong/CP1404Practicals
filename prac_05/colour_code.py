127023cce397ee951bcd7ae30eafc239400f7484
havkokfong <45163343+havkokfong@users.noreply.github.com>
2018-12-04 15:02:22 +0800

    update docstring

--- prac_05/colour_code.py
+++ prac_05/colour_code.py
@@ -1,4 +1,4 @@
-
+""" Programming II colour code """
 
 COLOUR = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "	#ffefdb", "AntiqueWhite2": "#eedfcc",
           "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6",
