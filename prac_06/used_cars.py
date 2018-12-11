ba217946e4c36ebd88013c4909a5aa5ec173ea71
Hav Kokfong <havkokfong@my.jcu.edu.au>
2018-12-11 14:08:23 +0800

    finish

---
+++ prac_06/used_cars.py
@@ -0,0 +1,22 @@
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+# Note that the import has a folder (module) in it.
+
+from prac_06.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car(180)
+    my_car.drive(30)
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+    print("\n")
+    limo = Car(100, "Ferrari")
+    limo.add_fuel(20)
+    print("fuel =", limo.fuel)
+    limo.drive(115)
+    print("odo =", limo.odometer)
+    print(limo)
+
+main()
