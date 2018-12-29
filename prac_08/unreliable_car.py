112f9ca718b5959db82f35b69b295b7f8542be8d
Hav Kokfong <havkokfong@my.jcu.edu.au>
2018-12-29 14:45:18 +0800

    Finished

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -1,13 +1,23 @@
 
 from prac_08.car import Car
-import random
 
-class Car(UnreliableCar):
+
+class UnreliableCar(Car):
 
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+        self.driven_distance = 0
+
+    def __str__(self):
+        return "{}, {} car reliability, {} driven distance".format(super().__str__(), self.reliability,
+                                                                   self.driven_distance)
 
     def drive(self, distance):
+        if distance < self.reliability:
+            drive_distance = super().drive(distance)
+            self.driven_distance += drive_distance
+            return drive_distance
+
 
 
