c30de86234765799c6210e05cc7ccaa7c6f006ff
Hav Kokfong <havkokfong@my.jcu.edu.au>
2019-01-08 14:49:59 +0800

    Completed

--- prac_09/wiki.py
+++ prac_09/wiki.py
@@ -5,8 +5,10 @@ def main():
     user_input = input("Please Enter your page title: ")
     while user_input != '':
         try:
-            result = wikipedia.summary(user_input)
-            print(result)
+            result = wikipedia.page(user_input)
+            print(result.title)
+            print(wikipedia.summary(result))
+            print(result.url)
         except wikipedia.exceptions.DisambiguationError as e:
             print(e.options)
         user_input = input("Please Enter your page title: ")
